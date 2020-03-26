8489a5f11d247f00d61e8553b116731aae25622a
tony363 <pysolver33@gmail.com>
2020-03-27 00:04:31 +0800

    bars closing

--- sample.py
+++ sample.py
@@ -17,7 +17,7 @@ import json
 import sys
 import struct
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from typing import Optional
@@ -26,45 +26,83 @@ from sklearn.model_selection import train_test_split
 
 
 fake_users_db = {
-    "johndoe":{
-        'username':'johndoe',
-        'full_name':'John Doe',
-        'email':'johndoe@example.com',
-        'hashed_password':'fakehashedsecret',
-        'disabled':False,
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
     },
-    "alice":{
-        'username':'alice',
-        'full_name':'Alice Wonderson',
-        'email':'alice@example.com',
-        'hashed_password':'fakehashedsecret2',
-        'disabled':True
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
     },
 }
 
 app = FastAPI()
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 
 class User(BaseModel):
     username: str
-    email: Optional[str] = None 
-    full_name: Optional[str] = None 
-    disabled: Optional[bool] = None 
+    email: str = None 
+    full_name: str = None 
+    disabled: bool = None 
+
+class UserInDB(User):
+    hashed_password: str
 
 
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+def get_user(db,username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
 
 def fake_decode_token(token):
-    return User(
-        username=token + "fakedecoded", email="sample@example.com", full_name="John Doe"
-    )
+    # this doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db,token)
+    return user
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authenitcation credentials",
+            headers={"WWW-Authenticate":"Bearer"}
+        )
     return user
 
-@app.get("/users/me")
-async def read_users_me(current_user: User = Depends(get_current_user)):
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400,detail="Inactive user")
     return current_user
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token":user.username,"token_type":"bearer"}
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+##############################################
+
 
 @app.get("/")
 async def root():
@@ -76,7 +114,7 @@ async def root():
     elif digit in used:
         return {"message":'this is a used digit'}
 
-@app.get("/prediction/{stock_id}")
+@app.post("/prediction/{stock_id}")
 async def predict(stock_id):
     loaded_model = xgb.Booster()
     loaded_model.load_model('xgbregression.model')
