66dd782b645580687aee244e2b13901acea83546
tony363 <pysolver33@gmail.com>
2020-04-30 22:18:57 +0800

    hmm,guess i should go into security then?

---
+++ finnhub_webhook_sample.py
@@ -0,0 +1,20 @@
+import requests
+
+# Register new webhook for earnings
+r = requests.post('https://finnhub.io/api/v1/webhook/add?token=bql9nt7rh5rfdbi8mhm0', json={'event': 'earnings', 'symbol': 'AAPL'})
+res = r.json()
+print(res)
+
+webhook_id = res['id']
+# List webhook
+r = requests.get('https://finnhub.io/api/v1/webhook/list?token=bql9nt7rh5rfdbi8mhm0')
+res = r.json()
+print(res)
+
+#Delete webhook
+r = requests.post('https://finnhub.io/api/v1/webhook/delete?token=bql9nt7rh5rfdbi8mhm0', json={'id': webhook_id})
+res = r.json()
+print(res)
+
+r = requests.get('https://finnhub.io/api/v1/stock/candle?symbol=^DJI&resolution=1&from=1572651390&to=1572910590&token=bql9nt7rh5rfdbi8mhm0')
+print(r.json())
--- finnhub_webstream.py
+++ finnhub_webstream.py
@@ -8,25 +8,8 @@ from secrets import *
 
 
 def on_message(ws,message):
+    print("this is the message")
     print(message)
-
-def on_error(ws,error):
-    print(error)
-
-def on_close(ws):
-    print("### closed ###")
-
-def on_open(ws):
-    # ws.send('{"type":"subscribe","symbol":"AAPL"}')
-    # ws.send('{"type":"subscribe","symbol":"AMZN"}')
-    # ws.send('{"type":"subscribe","symbol":"BINANCE:BTCUSDT"}')
-    # ws.send('{"type":"subscribe","symbol":"IC MARKETS:1"}')
-    ws.send('{"type":"subscribe","symbol":"^DJI"}')
-
-def pairs_trading_algo():
-    print('WTF')
-
-
     stock = 'DJI'
     sender_address = 'pythonemails333@gmail.com'
     sender_pass = input('please input password: ')
@@ -59,26 +42,51 @@ def pairs_trading_algo():
     done = 'mail sent'
     print(done)
 
-if __name__ == "__main__":
+def on_error(ws,error):
+    print(error)
+
+def on_close(ws):
+    print("### closed ###")
+
+def on_open(ws):
+    ws.send('{"type":"subscribe","symbol":"AAPL"}')
+    # ws.send('{"type":"subscribe","symbol":"AMZN"}')
+    # ws.send('{"type":"subscribe","symbol":"BINANCE:BTCUSDT"}')
+    # ws.send('{"type":"subscribe","symbol":"IC MARKETS:1"}')
+    ws.send('{"type":"subscribe","symbol":"^DJI"}')
+  
+
+def pairs_trading_algo():
+    print('WTF')
 
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp("wss://ws.finnhub.io?token=bql9nt7rh5rfdbi8mhm0",
                                 on_message=on_message,
                                 on_error=on_error,
                                 on_close=on_close)
-    
+
     ws.on_open = on_open
     ws.run_forever()
 
+    
+
+if __name__ == "__main__":
     stock = 'DJI'
   
     x = datetime.datetime.today()
-    y = x.replace(day=x.day+1, hour=18, minute=30, second=0, microsecond=0)
+    try:
+        y = x.replace(day=x.day+1, hour=22, minute=9, second=0, microsecond=0)
+    except Exception as e:
+        y = x.replace(day=x.day, hour=22,minute=9,second=0,microsecond=0)
     delta_t = y-x
 
     secs = delta_t.seconds+1
 
     t = Timer(secs, pairs_trading_algo)
     t.start()
+
+    
+
+   
 
     
