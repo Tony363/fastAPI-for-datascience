b47f832803aec83939a955665bc7315bab184742
tony363 <pysolver33@gmail.com>
2020-05-15 21:30:52 +0800

    updating

--- AWS_lamda/main.py
+++ AWS_lamda/main.py
@@ -1,5 +1,5 @@
 import random
-import socket
+
 import sys
 import os
 import time
@@ -10,10 +10,10 @@ import numpy as np
 import os
 import math
 import time
-import socket
+
 import json
 import sys
-import struct
+# import struct
 import jwt
 from jwt import PyJWTError
 from datetime import datetime,timedelta
